0feb34fa9b6c324910a9389240c8f992773ef5f7
jg13 <jonathan.griffiths@maiple.com>
2022-11-10 13:32:42 +0100

    Update pipeline-stack.ts

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,14 +1,13 @@
 import {
-    RemovalPolicy,
-    aws_codepipeline,
-    aws_codebuild,
-    aws_codepipeline_actions,
-    Stack,
-    StackProps,
-    aws_iam
-} from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-
+  aws_codebuild,
+  aws_codepipeline,
+  aws_codepipeline_actions,
+  aws_iam,
+  RemovalPolicy,
+  Stack,
+  StackProps,
+} from "aws-cdk-lib";
+import { Construct } from "constructs";
 
 interface PipelineProps extends StackProps {
   codeStarId: string;
@@ -18,166 +17,188 @@ export class PipelineStack extends Stack {
   constructor(scope: Construct, id: string, props: PipelineProps) {
     super(scope, id, props);
 
-    // Artifacts
-    const sourceOutput = new aws_codepipeline.Artifact("SourceOutput");
-    const unitTestBuildOutput = new aws_codepipeline.Artifact("UnittestBuildOutput");
-    const cdkBuildOutput = new aws_codepipeline.Artifact("CdkBuildOutput");
-
-    // Github connection
-    const sourceAction = new aws_codepipeline_actions.CodeStarConnectionsSourceAction({
-      actionName: "GitHub",
-      owner: "JonathanGriffiths94",
-      connectionArn: `arn:aws:codestar-connections:${this.region}:${this.account}:connection/${props.codeStarId}`,
-      repo: "aws-ci-cd-lambda-docker-test",
-      branch: "main",
-      output: sourceOutput
-    });
-
-    // CodeBuild for unit test
-    const unitTestCodeBuildProject = new aws_codebuild.PipelineProject(
-        this,
-        "UnitTestCodeBuildProject",
-        {
-          projectName: "UnitTestCodeBuildProject",
-          environment: {
-            buildImage: aws_codebuild.LinuxBuildImage.STANDARD_5_0
-          },
-          buildSpec: aws_codebuild.BuildSpec.fromObject({
-            version: "0.2",
-            phases: {
-              install: {
-                commands: ["pip install -r requirements.txt", "echo $CODE_COMMIT_ID"],
-              },
-              build: {
-                commands: ["python -m pytest -s -v unittest/test_lambda_logic.py"],
-              }
-            }
-          })
-        }
-    )
-
-    // Role for integration test
-    const role = new aws_iam.Role(this, "RoleForIntegrationTest", {
-      roleName: "RoleForIntegrationTest",
-      assumedBy: new aws_iam.ServicePrincipal("codebuild.amazonaws.com")
-    });
-
-    role.attachInlinePolicy(
-        new aws_iam.Policy(this, "CodeBuildReadCloudFormation", {
-          policyName: "CodeBuildReadCloudFormation",
-          statements: [
-              new aws_iam.PolicyStatement({
-                actions: ["cloudformation:*"],
-                resources: ["*"]
-              })
-          ]
-        })
-    )
-
-      // CodeBuild for integration test
-    const integTestCodeBuildProject = new aws_codebuild.PipelineProject(
+    // codebuild unitest
+    const unittestCodeBuild = new aws_codebuild.PipelineProject(
       this,
-      "CodeBuildIntegTest",
+      "CodeBuildUnittest",
       {
-        role: role,
         environment: {
-          buildImage: aws_codebuild.LinuxBuildImage.STANDARD_5_0
+          buildImage: aws_codebuild.LinuxBuildImage.STANDARD_5_0,
         },
         buildSpec: aws_codebuild.BuildSpec.fromObject({
           version: "0.2",
           phases: {
             install: {
               commands: [
-                  `SERVICE_URL=$(aws cloudformation describe-stacks --stack-name PreProdApplicationStack
-                  --query "Stacks[0].Outputs[OutputKey=='UrlPreProd'].OutputValue" --output text)`,
-                  "echo $SERVICE_URL",
-                  "pip install -r requirements.txt"
-              ]
+                "echo $CODE_COMMIT_ID",
+                "pip install -r requirements.txt",
+              ],
             },
             build: {
-              commands: ["python -m pytest -s -v integ_tests/test_service.py"],
-            }
-          },
-          artifacts: {}
-        })
-      }
-    )
-
-    // CodeBuild to build cdk stack
-    const cdkBuildProject = new aws_codebuild.PipelineProject(
-        this,
-        "CdkBuildProject",{
-          environment: {
-            buildImage: aws_codebuild.LinuxBuildImage.STANDARD_5_0
-          },
-          buildSpec: aws_codebuild.BuildSpec.fromObject({
-            version: "0.2",
-            phases: {
-              install: {
-                commands: ["npm install"]
-              },
-              build: {
-                commands: ["npm run cdk synth -- -o dist"]
-              }
+              commands: [
+                "python -m pytest -s -v unittests/test_lambda_logic.py",
+              ],
             },
-            artifacts: {
-              "base-directory": "dist",
-              files: ["*.template.json"]
-            }
-          })
-        }
+          },
+          artifacts: {},
+        }),
+      }
     );
 
-    // Unit test CodeBuild action
-    const unitTestBuildAction = new aws_codepipeline_actions.CodeBuildAction({
-      actionName: "DoUnitTest",
-      project: unitTestCodeBuildProject,
-      input: sourceOutput,
-      outputs: [unitTestBuildOutput],
+    // role for integration test
+    const role = new aws_iam.Role(this, "RoleForIntegerationTest", {
+      roleName: "RoleForIntegrationTest",
+      assumedBy: new aws_iam.ServicePrincipal("codebuild.amazonaws.com"),
+    });
+
+    role.attachInlinePolicy(
+      new aws_iam.Policy(this, "CodeBuildReadCloufFormation", {
+        policyName: "CodeBuildReadCloufFormation",
+        statements: [
+          new aws_iam.PolicyStatement({
+            actions: ["cloudformation:*"],
+            resources: ["*"],
+          }),
+        ],
+      })
+    );
+
+    // codebuild integration test
+    const integtestCodeBuild = new aws_codebuild.PipelineProject(
+      this,
+      "CodeBuildIntegTest",
+      {
+        role: role,
+        environment: {
+          buildImage: aws_codebuild.LinuxBuildImage.STANDARD_5_0,
+        },
+        buildSpec: aws_codebuild.BuildSpec.fromObject({
+          version: "0.2",
+          phases: {
+            install: {
+              commands: [
+                `SERVICE_URL=$(aws cloudformation describe-stacks --stack-name PreProdApplicationStack --query "Stacks[0].Outputs[?OutputKey=='UrlPreProd'].OutputValue" --output text)`,
+                "echo $SERVICE_URL",
+                "pip install -r requirements.txt",
+              ],
+            },
+            build: {
+              commands: ["python -m pytest -s -v integtests/test_service.py"],
+            },
+          },
+          artifacts: {},
+        }),
+      }
+    );
+
+    // codebuild cdk template
+    const cdkCodeBuild = new aws_codebuild.PipelineProject(
+      this,
+      "CodeBuildCdk",
+      {
+        environment: {
+          buildImage: aws_codebuild.LinuxBuildImage.STANDARD_5_0,
+        },
+        buildSpec: aws_codebuild.BuildSpec.fromObject({
+          version: "0.2",
+          phases: {
+            install: {
+              commands: ["npm install"],
+            },
+            build: {
+              commands: ["npm run cdk synth -- -o dist"],
+            },
+          },
+          artifacts: {
+            "base-directory": "dist",
+            files: ["*.template.json"],
+          },
+        }),
+      }
+    );
+
+    // source output
+    const sourceOutput = new aws_codepipeline.Artifact("SourceCode");
+    const cdkBuildOutput = new aws_codepipeline.Artifact("CdkBuildOutput");
+    const unitestCodeBuildOutput = new aws_codepipeline.Artifact(
+      "UnittestBuildOutput"
+    );
+    const preProdOutput = new aws_codepipeline.Artifact("PreProductOutput");
+
+    // source action
+    // const sourceAction = new aws_codepipeline_actions.CodeCommitSourceAction({
+    //   actionName: "CodeCommit",
+    //   repository: repo,
+    //   branch: "master",
+    //   output: sourceOutput,
+    //   variablesNamespace: "SourceVariables",
+    // });
+
+    // github source
+    const sourceAction =
+      new aws_codepipeline_actions.CodeStarConnectionsSourceAction({
+        actionName: "Github_Source",
+        owner: "JonathanGriffiths94",
+        connectionArn: `arn:aws:codestar-connections:${this.region}:${this.account}:connection/${props.codeStarId}`,
+        repo: "aws-ci-cd-lambda-docker-test",
+        branch: "main",
+        output: sourceOutput,
+      });
+
+    // build action
+    const unittestBuildAction = new aws_codepipeline_actions.CodeBuildAction({
       environmentVariables: {
         CODE_COMMIT_ID: {
-          value: sourceAction.variables.commitId
-        }
-      }
+          value: sourceAction.variables.commitId,
+        },
+      },
+      actionName: "DoUnitest",
+      project: unittestCodeBuild,
+      input: sourceOutput,
+      outputs: [unitestCodeBuildOutput],
     });
 
-
-    // Integration CodeBuild action
-    const integTestBuildAction = new aws_codepipeline_actions.CodeBuildAction({
-      actionName: "IntegTest",
-      project: integTestCodeBuildProject,
-      input: sourceOutput
-    });
-
-    // Cdk build action
+    // cdk build template
     const cdkBuild = new aws_codepipeline_actions.CodeBuildAction({
-        actionName: "BuildCfnTemplate",
-        project: cdkBuildProject,
-        input: sourceOutput,
-        outputs: [cdkBuildOutput]
-    })
+      actionName: "BuildCfnTemplate",
+      project: cdkCodeBuild,
+      input: sourceOutput,
+      outputs: [cdkBuildOutput],
+    });
 
-    // CodedDeploy to deploy pre-production api
-    const deployPreProd = new aws_codepipeline_actions.CloudFormationCreateUpdateStackAction({
+    // deploy preprod
+    const deployPreProd =
+      new aws_codepipeline_actions.CloudFormationCreateUpdateStackAction({
         actionName: "DeployPreProdApplication",
         templatePath: cdkBuildOutput.atPath(
-            "PreProdApplicationStack.template.json"
+          "PreProdApplicationStack.template.json"
         ),
         stackName: "PreProdApplicationStack",
-        adminPermissions: true
+        adminPermissions: true,
+        // variablesNamespace: "PreProdVariables",
+        // outputFileName: "PreProdOutputs",
+        // output: preProdOutput,
+      });
+
+    // build action
+    const integtestBuildAction = new aws_codepipeline_actions.CodeBuildAction({
+      actionName: "IntegTest",
+      project: integtestCodeBuild,
+      input: sourceOutput,
     });
 
-    // CodeDeploy to deploy production api
-    const deployProd = new aws_codepipeline_actions.CloudFormationCreateUpdateStackAction({
-    actionName: "DeployProdApplication",
-    templatePath: cdkBuildOutput.atPath(
-        "ProdApplicationStack.template.json"
-    ),
-    stackName: "ProdApplicationStack",
-    adminPermissions: true
-    });
+    // deploy product
+    const deployProd =
+      new aws_codepipeline_actions.CloudFormationCreateUpdateStackAction({
+        actionName: "DeployProd",
+        templatePath: cdkBuildOutput.atPath(
+          "ProApplicationStack.template.json"
+        ),
+        stackName: "ProApplicationStack",
+        adminPermissions: true,
+      });
 
-      // pipeline
+    // pipeline
     const pipeline = new aws_codepipeline.Pipeline(this, "CicdPipelineDemo", {
       pipelineName: "CicdPipelineDemo",
       crossAccountKeys: false,
@@ -188,7 +209,7 @@ export class PipelineStack extends Stack {
         },
         {
           stageName: "Unittest",
-          actions: [unitTestBuildAction],
+          actions: [unittestBuildAction],
         },
         {
           stageName: "BuildTemplate",
@@ -200,7 +221,7 @@ export class PipelineStack extends Stack {
         },
         {
           stageName: "IntegTest",
-          actions: [integTestBuildAction],
+          actions: [integtestBuildAction],
         },
         {
           stageName: "DeployProd",
@@ -209,7 +230,7 @@ export class PipelineStack extends Stack {
       ],
     });
 
-    // Remove artifact bucket on stack delete
-    pipeline.artifactBucket.applyRemovalPolicy(RemovalPolicy.DESTROY)
+    // destroy artifact bucket when deleling pipeline
+    pipeline.artifactBucket.applyRemovalPolicy(RemovalPolicy.DESTROY);
   }
 }
